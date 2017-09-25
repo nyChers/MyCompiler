f06d24ab0dbd5d992befdf92461f90a9f62d2966
nyChers <zny_chers@hotmail.com>
2017-09-26 00:12:09 +0800

    ~

--- LexicalAnalysis/base.cpp
+++ LexicalAnalysis/base.cpp
@@ -1,8 +1,9 @@
-/**********************************
-* File:     base.cpp
-*
-* Author:   NingYu Zhang
-*
-* Email:
-*********************************/
+/**
+ * @Author: NingYu Zhang <nyChers>
+ * @Date:   2017-09-25T23:27:24+08:00
+ * @Email:  zny_chers@hotmail.com
+ * @Filename: base.cpp
+ * @Last modified by:   nyChers
+ * @Last modified time: 2017-09-26T00:11:05+08:00
+ */
 #include "base.h"
